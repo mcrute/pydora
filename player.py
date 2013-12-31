d5f0f7d7848da7655a2ef829315f9e304e9fd5fc
Mike Crute <mcrute@gmail.com>
2013-12-30 20:31:34 -0500

    Add help

--- player.py
+++ player.py
@@ -20,6 +20,19 @@ from pandora.utils import Colors, Screen
 
 class PlayerApp:
 
+    CMD_MAP = {
+        'n': ('play next song', 'skip_song'),
+        'p': ('pause/resume song', 'pause_song'),
+        's': ('stop playing station', 'stop_station'),
+        'd': ('dislike song', 'dislike_song'),
+        'u': ('like song', 'like_song'),
+        'b': ('bookmark song', 'bookmark_song'),
+        'a': ('bookmark artist', 'bookmark_artist'),
+        'S': ('sleep song for 30 days', 'sleep_song'),
+        'Q': ('quit player', 'quit'),
+        '?': ('display this help', 'help'),
+    }
+
     def __init__(self):
         self.client = APIClient.from_settings_dict(settings.SETTINGS)
         self.player = Player(self, sys.stdin)
@@ -36,54 +49,70 @@ class PlayerApp:
         return self.stations[Screen.get_integer('Station: ')]
 
     def play(self, song):
-        """Play callback, prints song name
+        """Play callback
         """
         print('{} by {}'.format(Colors.blue(song.song_name),
             Colors.yellow(song.artist_name)))
 
+    def skip_song(self, song):
+        self.player.stop()
+
+    def pause_song(self, song):
+        self.player.pause()
+
+    def stop_station(self, song):
+        self.player.end_station()
+
+    def dislike_song(self, song):
+        song.thumbs_down()
+        Screen.print_success('Track disliked')
+        self.player.stop()
+
+    def like_song(self, song):
+        song.thumbs_up()
+        Screen.print_success('Track liked')
+
+    def bookmark_song(self, song):
+        song.bookmark_song()
+        Screen.print_success('Bookmarked song')
+
+    def bookmark_artist(self, song):
+        song.bookmark_artist()
+        Screen.print_success('Bookmarked artist')
+
+    def sleep_song(self, song):
+        song.sleep()
+        Screen.print_success('Song will not be played for 30 days')
+        self.player.stop()
+
+    def quit(self, song):
+        self.player.end_station()
+        sys.exit(0)
+
+    def help(self, song):
+        print("")
+        print("\n".join([
+            "\t{} - {}".format(k, v[0]) for k, v in self.CMD_MAP.items()
+        ]))
+        print("")
+
     def input(self, input, song):
         """Input callback, handles key presses
         """
-        if input == 'n':
-            self.player.stop()
-        elif input == 'p':
-            self.player.pause()
-        elif input == 's':
-            self.player.end_station()
-        elif input == 'd':
-            song.thumbs_down()
-            Screen.print_success('Track disliked')
-            self.player.stop()
-        elif input == 'u':
-            song.thumbs_up()
-            Screen.print_success('Track disliked')
-        elif input == 'b':
-            song.bookmark_song()
-            Screen.print_success('Bookmarked song')
-        elif input == 'a':
-            song.bookmark_artist()
-            Screen.print_success('Bookmarked artist')
-        elif input == 'S':
-            song.sleep()
-            Screen.print_success('Song will not be played for 30 days')
-            self.player.stop()
-        elif input == 'Q':
-            self.player.end_station()
-            sys.exit(0)
+        try:
+            cmd = getattr(self, self.CMD_MAP[input][1])
+        except (IndexError, KeyError):
+            return Screen.print_error('Invalid command!')
+
+        cmd(song)
 
     def pre_poll(self):
-        """Disable echo before polling stdin
-        """
         Screen.set_echo(False)
 
     def post_poll(self):
-        """Re-enable echo after polling stdin
-        """
         Screen.set_echo(True)
 
     def run(self):
-        """Main run loop of the program
-        """
         self.client.login(settings.USERNAME, settings.PASSWORD)
         self.stations = self.client.get_station_list()
 
