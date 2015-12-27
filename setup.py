582334a17f0a08e1106cf657da1ca87108feca0b
Mike Crute <mcrute@gmail.com>
2015-12-27 08:55:06 -0800

    Release 1.6.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydora",
-    version="1.6.2",
+    version="1.6.3",
     description="Python wrapper for Pandora API",
     long_description=open("README.rst", "r").read(),
     author="Mike Crute",
