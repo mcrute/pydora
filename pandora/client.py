79af8c5a61de0feb54bd14c17fb81dd742c04e2c
John Cass <john.cass77@gmail.com>
2016-06-07 07:23:57 +0200

    Add support for searching genre stations. (#45)
    Add support for searching genre stations.

--- pandora/client.py
+++ pandora/client.py
@@ -174,12 +174,18 @@ class APIClient(BaseAPIClient):
         return self("bookmark.deleteArtistBookmark",
                     bookmarkToken=bookmark_token)
 
-    def search(self, search_text):
+    def search(self, search_text,
+               include_near_matches=False,
+               include_genre_stations=False):
         from .models.pandora import SearchResult
 
-        return SearchResult.from_json(self,
-                                      self("music.search",
-                                           searchText=search_text))
+        return SearchResult.from_json(
+            self,
+            self("music.search",
+                 searchText=search_text,
+                 includeNearMatches=include_near_matches,
+                 includeGenreStations=include_genre_stations)
+        )
 
     def add_feedback(self, track_token, positive):
         return self("station.addFeedback",
