d16320a8ff2d55096447a1c94d8e44b0c41dc56f
Mike Crute <mcrute@gmail.com>
2016-06-11 16:15:24 -0700

    Enable wheel builds

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=[
         "py_release_tools",
+        "wheel",
     ],
     install_requires=[
         "pycrypto>=2.6.1",
